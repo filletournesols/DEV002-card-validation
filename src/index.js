7ab204a8cef4bcee3bd3613cfae8e1ae947852ae
Domynike Galarce <dgalarcegonzalez@gmail.com>
2022-10-28 13:34:31 -0300

    borra primer dígito

--- src/index.js
+++ src/index.js
@@ -5,19 +5,25 @@ console.log(validator);
 //se puede evitar repetir tanto const si, en vez de poner punto y coma, se usa una coma
 //const card = document.querySelector("#card");
 
-const cardForm = document.querySelector("#cardForm");
-
-function handleForm(event) {
-   event.preventDefault();
-   validate()
-} 
-cardForm.addEventListener('submit', handleForm);
-
 const cardNumber = document.querySelector("#inputNumber");
 const cardName = document.querySelector("#inputName");
 const textCardNumber = document.querySelector("#textNumber .number");
 const textCardName = document.querySelector("#textName .name");
 //const button = document.querySelector("#button");
+
+const cardForm = document.querySelector("#cardForm");
+
+function handleForm(event) {
+   event.preventDefault();
+   const inputValue = cardNumber.value
+   const finalValidation = validator.isValid(inputValue)     ///imitar
+   if (finalValidation){
+      alert ("Tarjeta Válida");
+   } else {
+      alert ("Tarjeta inválida");
+   }
+} 
+//cardForm.addEventListener('submit', handleForm);
 
 //""-> para describir el consolo.log; + o coma para agregar lo que se quiere en el console.log
 //console.log("inputtext"+cardNumber) 
@@ -30,17 +36,18 @@ cardNumber.addEventListener("keyup", (e) => {
    //eliminar espacios en blanco
    .replace(/\s/g, "")
    //eliminar letras
-   .replace(/\D/g, "")
+   //.replace(/\D/g, "")
    //espacios cada 4 números
    .replace(/([0-9]{4})/g, "$1 ")
    //elimina el último espacio
    .trim();
+   
+   validator.maskify(inputValue);
+   console.log("maskify "+validator.maskify(inputValue));
 
-   textCardNumber.textContent = inputValue;
-   maskify(inputValue);
-   console.log("maskify");
+   textCardNumber.textContent = validator.maskify(inputValue);
 
-   if(inputValue == "") {
+   if(validator.maskify(inputValue) == "") {
     textCardNumber.textContent = "0000 0000 0000 0000";
     //logo en la tarjeta
     //logoMarca.innerHTML = "";
@@ -68,61 +75,8 @@ cardNumber.addEventListener("keyup", (e) => {
      //image.src = "imágenes/logos/discover.jpeg";
      //logoMarca.appendChild(image);
      //}
+
 })
-
-
-  // validación
-
-   function validate(){
-   const number = (Array.from(document.getElementById("inputNumber").value.replace(/\s/g, "")));
-   console.log("number without spaces "+number);
-   const reverseNumbers = number.reverse();
-   console.log("reverse number "+reverseNumbers);
-
-   const evenPosition = reverseNumbers.map((item, index) => {
-      if((index + 1) % 2 === 0){
-         let numberDouble = item * 2
-         if(numberDouble > 9){
-           const numberDoubleString = numberDouble-9
-           //restar 9 a los números mayores de 9
-           //sumar ambos números
-
-           //const sumNumber = parseInt(numberDoubleString.charAt(0)) + parseInt(numberDoubleString.charAt(15))
-           //return sumNumber
-           return numberDoubleString
-         } else {
-            return numberDouble
-         }
-      } else {
-            return item
-         }
-   })
-   console.log("evenPosition "+evenPosition);
-
-   let sum = 0;
-   //evenPosition.map((item, index) => {
-   //sum = sum + parseInt(evenPosition);
-   //})
-
-
-   for (var i = 0; i < evenPosition.length; i++) {
-      sum = sum + parseInt(evenPosition[i]);
-    }
-
-   console.log("sum "+sum);
-   console.log("mod "+sum % 10);
-
-   if(sum % 10 === 0) {
-      alert("Correcto")
-   } else {
-      alert("meh")
-   }
- }
-   
-   function maskify(cardNumber) {
-      console.log(cardNumber);
-      
- }
 
 cardName.addEventListener("keyup", (e) => {
    let inputValue = e.target.value;
@@ -138,8 +92,9 @@ cardName.addEventListener("keyup", (e) => {
    }
 });
 
+cardForm.addEventListener('submit', handleForm);
 
-
+//tarjetas válidas
 //5325477020691868
 //5332213098448506
 //4289854871191825
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,58 @@
 const validator = {
-  // ...
+  maskify: function (inputValue) {
+    //if (inputValue.length < 6) {return inputValue};
+    const last4Characters = inputValue.substr(-4);
+    //const firstCharacter = inputValue.substr(0,1);
+    const maskingCharacters = inputValue.substr(0, inputValue.length - 5).replace(/\d|[a-zA-Z]/g, "#");
+    return `${maskingCharacters}${last4Characters}`;
+    //${firstCharacter}
+  },
+ 
+  // reemplaza dígitos 0-9: /\d/g
+  // reemplaza letras [a-zA-Z]
+
+  isValid: function (inputValue){
+    const number = (Array.from(inputValue.replace(/\s/g, "")));
+    //document.getElementById("inputNumber").value
+    console.log("number without spaces "+number);
+    const reverseNumbers = number.reverse();
+    console.log("reverse number "+reverseNumbers);
+ 
+    const evenPosition = reverseNumbers.map((item, index) => {
+       if((index + 1) % 2 === 0){
+          let numberDouble = item * 2
+          if(numberDouble > 9){
+            const numberDoubleString = numberDouble-9
+            return numberDoubleString
+          } else {
+             return numberDouble
+          }
+       } else {
+             return item
+          }
+    })
+    console.log("evenPosition "+evenPosition);
+ 
+    let sum = 0;
+    //evenPosition.map((item, index) => {
+    //sum = sum + parseInt(evenPosition);
+    //})
+ 
+    for (var i = 0; i < evenPosition.length; i++) {
+       sum = sum + parseInt(evenPosition[i]);
+     }
+ 
+    console.log("sum "+sum);
+    console.log("mod "+sum % 10);
+ 
+    if(sum % 10 === 0) {
+      return true
+       //alert("Tarjeta válida")
+    } else {
+      return false
+       //alert("Tarjeta inválida")
+    }
+  }
 };
 
 export default validator;
